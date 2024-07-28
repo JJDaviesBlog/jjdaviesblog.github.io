3b6d076b010f0af869411e54315a1d0d5c0204b2
JJDaviesBlog <jjdblogs@outlook.com>
2024-07-28 20:39:12 +0100

    Add files via upload

---
+++ backend/creating_posts.js
@@ -0,0 +1,24 @@
+idx = 1
+
+function addData() {
+    let user = `JessicaD57_${idx}`;
+    idx += 1;
+    let smth = `${document.getElementById("title").value}, ${document.getElementById("blog").value}`;
+
+    if (document.getElementById("title").value == "" || document.getElementById("blog").value == "") {
+        document.getElementById("storage").innerHTML = "Please enter some text.";
+        return;
+
+    } else {
+        // allows multiple blogs to be published by one user
+        localStorage.setItem(user, smth);
+        document.getElementById("storage").innerHTML = "Data added!";
+        console.log(localStorage);
+    }
+}
+
+function clearStorage() {
+    localStorage.clear();
+    document.getElementById("storage").innerHTML = "All data removed!";
+    console.log(localStorage);
+}
